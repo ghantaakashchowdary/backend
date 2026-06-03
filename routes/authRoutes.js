8c14855712ca4cdb82a4af019c640c97b7949449
akash <akashghanta22@gmail.com>
2026-06-03 15:48:27 +0530

    Update: New changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,21 @@ const router = express.Router();
 
 const { register, login } = require("../controllers/authController");
 
+const auth = require("../middleware/auth");
+const role = require("../middleware/role");
+
 router.post("/register", register);
 router.post("/login", login);
+
+router.get(
+  "/admin",
+  auth,
+  role("admin"),
+  (req, res) => {
+    res.json({
+      message: "Welcome Admin",
+    });
+  }
+);
+
 module.exports = router;
